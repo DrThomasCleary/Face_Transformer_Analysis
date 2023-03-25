623949d30e5007f3bd9c1e2937230eeb57797bbd
DrThomasCleary <118690399+DrThomasCleary@users.noreply.github.com>
2023-03-25 19:42:42 +0000

    transformer working!!

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 #importing libraries
 from facenet_pytorch import MTCNN
 import torch
-from torchvision import datasets
+from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import os
 import numpy as np
 import time
 import math
 import importlib.util
-import matplotlib
-matplotlib.use('TkAgg')  # or another appropriate backend, like 'Qt5Agg', 'GTK3Agg', etc.
 import matplotlib.pyplot as plt
+from torchvision.transforms import Resize
 
+
+resize = Resize((112, 112))
+
+use_mtcnn = False  # Change this to False if you want to exclude MTCNN
+to_tensor = transforms.ToTensor()
+
+if use_mtcnn:
+    mtcnn = MTCNN(image_size=112, margin=24, keep_all=False, min_face_size=50)
+else:
+    mtcnn = None
 
 # Import the 'perform_val' function from the 'util.utils' module
 util_path = "/Users/br/Software/Machine_learning/Face-Transformer/util/utils.py"
@@ -20,22 +29,24 @@ utils_module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(utils_module)
 perform_val = utils_module.perform_val
 
-vit_face_path = "/Users/br/Software/Machine_learning/Face-Transformer/vit_pytorch/vit_face.py"
-spec = importlib.util.spec_from_file_location("vit_face", vit_face_path)
-vit_face_module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(vit_face_module)
-ViT_face = vit_face_module.ViT_face
+vits_face_path = "/Users/br/Software/Machine_learning/Face-Transformer/vit_pytorch/vits_face.py"
+spec = importlib.util.spec_from_file_location("vits_face", vits_face_path)
+vits_face_module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(vits_face_module)
+ViTs_face = vits_face_module.ViTs_face
 
 # Initialize the ViT_face model
-model = ViT_face(
+model = ViTs_face(
     image_size=112,
     patch_size=8,
-    loss_type='ArcFace',
+    loss_type='CosFace',
     GPU_ID=torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'),
     num_class=93431,
     dim=512,
     depth=20,
     heads=8,
+    pad = 2,
+    ac_patch_size = 8,
     mlp_dim=2048,
     dropout=0.1,
     emb_dropout=0.1
@@ -59,6 +70,19 @@ matched_loader = DataLoader(matched_dataset, collate_fn=lambda x: x[0])
 mismatched_dataset = datasets.ImageFolder('/Users/br/Software/Machine_learning/MTCNN_face_transformer/LFW_dataset/small_samples/mismatched_faces_test')
 mismatched_dataset.idx_to_class = {i: c for c, i in mismatched_dataset.class_to_idx.items()}
 mismatched_loader = DataLoader(mismatched_dataset, collate_fn=lambda x: x[0])
+
+def calculate_accuracy(labels, distances, threshold):
+    correct_predictions = 0
+    total_predictions = len(labels)
+
+    for i in range(len(distances)):
+        if labels[i] == 1 and distances[i] <= threshold:
+            correct_predictions += 1
+        if labels[i] == 0 and distances[i] > threshold:
+            correct_predictions += 1
+
+    accuracy = correct_predictions / total_predictions
+    return accuracy
 
 def calculate_far_frr(labels, distances, threshold):
     num_positive = np.sum(labels)
@@ -99,18 +123,15 @@ for folder in os.listdir(matched_dataset.root):
             image_path = os.path.join(folder_path, filename)
             image = datasets.folder.default_loader(image_path)
             images.append(image)
-        # detect faces and generate embeddings for the two images in the folder
+        # generate embeddings for the two images in the folder
         if len(images) == 2:
             embeddings = []
             for i in range(2):
                 start_time_matched_resnet = time.time()
-                face, face_prob = mtcnn(images[i], return_prob=True)
-                if face is not None and face_prob > 0.73:
-                    emb = model(face.unsqueeze(0))
-                    embeddings.append(emb.detach())
-                else:
-                    print(f"No face detected in {os.path.basename(image_path)}")
-                    break
+                resized_image = resize(images[i])
+                tensor_image = to_tensor(resized_image)
+                emb = model(tensor_image.unsqueeze(0))
+                embeddings.append(emb.detach())
                 elapsed_time_matched_resnet = time.time() - start_time_matched_resnet
                 resnet_time += elapsed_time_matched_resnet
                 n_operations += 1
@@ -119,6 +140,7 @@ for folder in os.listdir(matched_dataset.root):
                 matched_name_list.extend([folder] * 2)
             else:
                 print(f"Not enough faces detected in {folder_path}")
+
             
 
 # generate embeddings for the dataset
@@ -126,20 +148,20 @@ mismatched_embedding_list = []
 mismatched_name_list = []
 for image, index in mismatched_loader:
     start_time__mismatched_resnet = time.time()
-    face, face_prob = mtcnn(image, return_prob=True)
-    if face is not None and face_prob > 0.73:
-        emb = model(face.unsqueeze(0))
-        mismatched_embedding_list.append(emb.detach())
-        mismatched_name_list.append(mismatched_dataset.idx_to_class[index])
-        elapsed_time_mismatched_resnet = time.time() - start_time__mismatched_resnet
-        resnet_time += elapsed_time_mismatched_resnet
-        n_operations += 1
-    else:
-        print("No face detected in image:", index)
+    resized_image = resize(image)
+    tensor_image = to_tensor(resized_image)
+    emb = model(tensor_image.unsqueeze(0))
+    mismatched_embedding_list.append(emb.detach())  
+    mismatched_name_list.append(mismatched_dataset.idx_to_class[index]) 
+    elapsed_time_mismatched_resnet = time.time() - start_time__mismatched_resnet
+    resnet_time += elapsed_time_mismatched_resnet
+    n_operations += 1
+
 
 if len(mismatched_embedding_list) % 2 != 0:
     mismatched_embedding_list.pop()
     mismatched_name_list.pop()
+
 
 dist_matched = [torch.dist(matched_embedding_list[i], matched_embedding_list[i + 1]).item() for i in range(0, len(matched_embedding_list), 2)]
 dist_mismatched = [torch.dist(mismatched_embedding_list[i], mismatched_embedding_list[i + 1]).item() for i in range(0, len(mismatched_embedding_list), 2)]
@@ -147,20 +169,22 @@ dist_mismatched = [torch.dist(mismatched_embedding_list[i], mismatched_embedding
 distances = dist_matched + dist_mismatched
 labels = [1] * len(dist_matched) + [0] * len(dist_mismatched)
 
-# Find the EER threshold and EER
-thresholds = np.linspace(0, 2, 1000)
+accuracies = []
+thresholds = np.linspace(0.1, 1.6, 5000)
 FARs = []
 FRRs = []
-
 for threshold in thresholds:
     FAR, FRR = calculate_far_frr(labels, distances, threshold)
+    accuracy = calculate_accuracy(labels, distances, threshold)
     FARs.append(FAR)
     FRRs.append(FRR)
+    accuracies.append(accuracy)
 
+max_accuracy_index = np.argmax(accuracies)
+max_accuracy_threshold = thresholds[max_accuracy_index]
+max_accuracy = accuracies[max_accuracy_index]
 eer_index = np.argmin(np.abs(np.array(FARs) - np.array(FRRs)))
-eer_threshold = thresholds[eer_index]
 eer = (FARs[eer_index] + FRRs[eer_index]) / 2
-print("EER Threshold:", eer_threshold)
 print("EER:", eer)
 
 # Calculate the accuracy and F1 score of the model
@@ -176,7 +200,7 @@ for i in range(0, len(matched_embedding_list), 2):
     dist = torch.dist(emb1, emb2).item()
     is_match = matched_name_list[i] == matched_name_list[i + 1]
     true_matched.append(is_match)
-    pred_matched.append(dist < eer_threshold)
+    pred_matched.append(dist < max_accuracy_threshold)
 
 # Compare embeddings and calculate metrics for mismatched faces
 for i in range(0, len(mismatched_embedding_list), 2):
@@ -185,7 +209,7 @@ for i in range(0, len(mismatched_embedding_list), 2):
     dist = torch.dist(emb1, emb2).item()
     is_mismatch = mismatched_name_list[i] != mismatched_name_list[i + 1]
     true_mismatched.append(is_mismatch)
-    pred_mismatched.append(dist > eer_threshold)
+    pred_mismatched.append(dist > max_accuracy_threshold)
 
 # True positives
 matched_correctly = 0
@@ -223,8 +247,11 @@ accuracy = (matched_correctly + mismatched_correctly) / (matched_correctly + mis
 precision = matched_correctly / (matched_correctly + matched_incorrectly)
 recall = matched_correctly / (matched_correctly + mismatched_incorrectly)
 f1 = 2 * precision * recall / (precision + recall)
-
+pred_matched.append(dist < max_accuracy_threshold)
+pred_mismatched.append(dist > max_accuracy_threshold)
 average_resnet_time = resnet_time / n_operations
+
+print("Max Accuracy Threshold:", max_accuracy_threshold)
 print("Average InceptionResnetV1 time:", average_resnet_time)
 print("Matched_Correctly: ", matched_correctly)
 print("Mismatched_Correctly: ", mismatched_correctly)
@@ -251,7 +278,7 @@ ax.scatter(dist_matched_incorrectly, add_jitter([1] * len(dist_matched_incorrect
 ax.scatter(dist_mismatched_incorrectly, add_jitter([0] * len(dist_mismatched_incorrectly), jitter_amount), c='orange', alpha=0.5, label='Mismatched_Incorrectly')
 
 # EER threshold
-ax.axvline(x=eer_threshold, color='purple', linestyle='--', label='EER Threshold')
+ax.axvline(x=max_accuracy_threshold, color='purple', linestyle='--', label='Max_accuracy Threshold')
 
 ax.set_xlabel('Distance')
 ax.set_yticks([0, 1, 2, 3])
